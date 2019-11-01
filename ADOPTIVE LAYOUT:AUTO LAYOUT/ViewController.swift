de7dc08440276ed14d49290f7556dad99362cd30
Syed.Reshma Ruksana <reshmasmile468@achiever-8.local>
2019-11-01 13:06:02 +0530

    Initial Commit

---
+++ ADOPTIVE LAYOUT:AUTO LAYOUT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ADOPTIVE LAYOUT:AUTO LAYOUT
+//
+//  Created by Syed.Reshma Ruksana on 01/11/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
